b13583f785b25b03706f932699e4b47d4961b1a6
ihanna <22100595@handong.ac.kr>
2022-09-05 10:50:52 +0900

    Word_getter,setter

---
+++ src/main/java/Word.java
@@ -0,0 +1,40 @@
+public class Word {
+    private int id; //같은 class 내에서 접근 가능
+    private int level;
+    private String word;
+    private String meaning;
+
+    Word(){}
+    Word(int id, int level, String word, String meaning){
+        this.id = id;
+        this.level = level;
+        this.word = word;
+        this.meaning = meaning;
+    } //생성자
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public void setWord(String word) {
+        this.word = word;
+    }
+    public String getWord() {
+        return word;
+    }
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+    public String getMeaning() {
+        return meaning;
+    }
+
+}
