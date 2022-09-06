f23328ac4763baad34940d148c9d640a2483716a
ihanna <22100595@handong.ac.kr>
2022-09-06 18:04:16 +0900

    updateItem 함수 생성

--- src/main/java/WordCRUD.java
+++ src/main/java/WordCRUD.java
@@ -49,4 +49,37 @@ public class WordCRUD implements ICRUD{
         }
         System.out.println("--------------------");
     }
+
+    public void deleteItem(){
+
+    }
+
+    public ArrayList<Integer> listAll(String keyword){
+        ArrayList<Integer> idlist = new ArrayList<>();
+        int j=0;
+        System.out.println("--------------------");
+        for(int i = 0; i<list.size(); i++){
+            String word = list.get(i).getWord();
+            if(!word.contains(keyword)) continue;
+            System.out.print((j+1) + " ");
+            System.out.println(list.get(i).toString());
+            idlist.add(i);
+            j++;
+        }
+        System.out.println("--------------------");
+        return idlist;
+    }
+    public void updateItem(){ //데이터 수정
+        System.out.print("=> 수정할 단어 검색 : ");
+        String keyword = s.next();
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 수정할 번호 선택 : ");
+        int id = s.nextInt();
+        s.nextLine();
+        System.out.print("=> 뜻 입력 : ");
+        String meaning = s.nextLine();// 공백 포함 입력 받기
+        Word word = list.get(idlist.get(id-1));
+        word.setMeaning(meaning);
+        System.out.println("단어가 수정되었습니다. ");
+    }
 }//ICRUD를 구현한 구현체
