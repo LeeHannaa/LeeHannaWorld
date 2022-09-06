9b441df99dc43846b0cac418bc958b206033670a
ihanna <22100595@handong.ac.kr>
2022-09-06 18:50:41 +0900

    toFileString 함수

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -46,4 +46,7 @@ public class Word {
                 + meaning;
         return str;
     }
+    public String toFileString(){
+        return this.level + "|" + this.word + "|" + this.meaning;
+    }
 }
