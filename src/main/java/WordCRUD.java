f5890ea854469e5e7d5add25cca35394b0b9f1cf
ihanna <22100595@handong.ac.kr>
2022-09-06 18:49:57 +0900

    saveFile 파일 저장하기

--- src/main/java/WordCRUD.java
+++ src/main/java/WordCRUD.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -118,6 +115,19 @@ public class WordCRUD implements ICRUD{
             br.close();
             System.out.println("==>" + count + "개 로딩 완료!!!");
         } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void saveFile(){
+        try {
+            PrintWriter pr = new PrintWriter(new FileWriter(fname));
+            for(Word one : list){
+                pr.write(one.toFileString() + "\n");
+            }
+            pr.close();
+            System.out.println("==> 데이터 저장 완료!!! ");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
