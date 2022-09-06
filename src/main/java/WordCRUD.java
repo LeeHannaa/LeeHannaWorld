31f7e01324bc8e02e6b22e46624caef20d623dce
ihanna <22100595@handong.ac.kr>
2022-09-06 19:15:11 +0900

    searchWord 구현

--- src/main/java/WordCRUD.java
+++ src/main/java/WordCRUD.java
@@ -149,4 +149,10 @@ public class WordCRUD implements ICRUD{
         int level = s.nextInt();
         listAll(level);
     }
+
+    public void searchWord() {
+        System.out.print("=> 원하는 단어는? ");
+        String keyword = s.next();
+        listAll(keyword);
+    }
 }//ICRUD를 구현한 구현체
