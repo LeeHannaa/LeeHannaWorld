942673d2fc780e06b03f5bcbadd1bc0802a2bb18
ihanna <22100595@handong.ac.kr>
2022-09-06 19:17:13 +0900

    정리

--- src/main/java/WordManager.java
+++ src/main/java/WordManager.java
@@ -27,10 +27,10 @@ public class WordManager {
         while (true) {
             int menu = selectMenu();
             if (menu == 0) break;
-            if (menu == 4) { wordCRUD.addItem();}
             else if (menu == 1) {wordCRUD.listAll();}
             else if (menu == 2) {wordCRUD.searchLevel();}
             else if (menu == 3) {wordCRUD.searchWord();}
+            else if (menu == 4) { wordCRUD.addItem();}
             else if (menu == 5) {wordCRUD.updateItem();}
             else if (menu == 6) {wordCRUD.deleteItem();}
             else if (menu == 7) {wordCRUD.saveFile();}
